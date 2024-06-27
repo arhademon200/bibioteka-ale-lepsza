cb0eae0d3b6fc6479e5b285debeed5950ac50fa3
arhademon200 <85450617+arhademon200@users.noreply.github.com>
2024-06-27 21:57:39 +0200

    Update mybooks.js

--- mybooks.js
+++ mybooks.js
@@ -1,7 +1,7 @@
 app.get('/mybooks', checkAuthenticated, async (req, res) => {
   try {
     const books = await Books.find({ borrower: req.user._id });
-    res.render('mybooks', { name: req.user.name, books });
+    res.json({ books });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
